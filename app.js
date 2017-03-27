22dbecbd80a1c64318d91ab62b29410c33169f63
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-27 15:54:49 +0100

    added lesson 5 code

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Shaun',
-        job: 'Ninja',
-        website: 'http://www.thenetninja.co.uk'
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
-        greet: function(time){
-            return 'Good ' + time + ', ' + this.name;
+        add: function(inc){
+            this.age += inc;
+        },
+        subtract: function(dec){
+            this.age -= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
