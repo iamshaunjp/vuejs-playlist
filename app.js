96bf55837edfd10fa0ae55fc07faec8538d70596
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-27 18:00:13 +0100

    added lesson 10 code

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 new Vue({
     el: '#vue-app',
     data: {
-        a: 0,
-        b: 0,
-        age: 20
+        available: false,
+        nearby: false
     },
     methods: {
-        /*addToA: function(){
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-            console.log('addToB');
-            return this.b + this.age;
-        }*/
+
     },
     computed: {
-        addToA: function(){
-            console.log('addToA');
-            return this.a + this.age;
-        },
-        addToB: function(){
-            console.log('addToB');
-            return this.b + this.age;
+        compClasses: function(){
+            return{
+                available: this.available,
+                nearby: this.nearby,
+            }
         }
     }
 });
