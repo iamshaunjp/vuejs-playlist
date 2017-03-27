2588e455bd115addea9f4036d49430e93318abb1
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-27 16:26:03 +0100

    added lesson 7 code

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age: 25,
-        x: 0,
-        y: 0
+
     },
     methods: {
-        add: function(inc){
-            this.age += inc;
+        logName: function(){
+            console.log('you entered your name');
         },
-        subtract: function(dec){
-            this.age -= dec;
-        },
-        updateXY: function(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function(){
-            alert('you clicked me');
+        logAge: function(){
+            console.log('you entered your age');
         }
     }
 });
