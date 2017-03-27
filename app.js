64a1d6a4fcd76a7a443733f3e37b19923c47d9d6
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-27 22:16:34 +0100

    added lesson 11 code

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 new Vue({
     el: '#vue-app',
     data: {
-        available: false,
-        nearby: false
+        error: false,
+        success: false
     },
     methods: {
 
     },
     computed: {
-        compClasses: function(){
-            return{
-                available: this.available,
-                nearby: this.nearby,
-            }
-        }
+
     }
 });
