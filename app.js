0c19db6f75ebdd1d8b8b18d29291bc192b32b6ac
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-27 16:09:50 +0100

    added lesson 6 code

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ new Vue({
         updateXY: function(event){
             this.x = event.offsetX;
             this.y = event.offsetY;
+        },
+        click: function(){
+            alert('you clicked me');
         }
     }
 });
