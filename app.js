049c0e6cedcb952c588e0ccdac743f93e1765d6a
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-27 15:18:08 +0100

    added lesson 3 code

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Shaun'
+        name: 'Shaun',
+        job: 'Ninja'
+    },
+    methods: {
+        greet: function(time){
+            return 'Good ' + time + ', ' + this.name;
+        }
     }
 });
