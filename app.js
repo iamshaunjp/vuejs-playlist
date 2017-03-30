cda1fe7f084590ac41499ea86788c0621e59e95f
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-30 09:46:52 +0100

    lesson 12 code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 new Vue({
     el: '#vue-app',
     data: {
-        error: false,
-        success: false
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name: 'Ryu', age: 25 },
+            { name: 'Yoshi', age: 35 },
+            { name: 'Ken', age: 55 }
+        ]
     },
     methods: {
 
