488233dabd1014c1cd3aecddf81b362d7e5a3174
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-30 15:20:10 +0100

    added lesson 15 code

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-new Vue({
-    el: '#vue-app',
-    data: {
-        health: 100,
-        ended: false
-    },
-    methods: {
-        punch: function(){
-            this.health -= 10;
-            if ( this.health <= 0 ){
-                this.ended = true;
-            }
-        },
-        restart: function(){
-            this.health = 100;
-            this.ended = false;
-        }
-    },
-    computed: {
+Vue.component('greeting', {
+    template: '<p>Hey there, I am a re-usable component</p>'
+});
 
-    }
+new Vue({
+    el: '.vue-app-one'
+    /*template: '<p>I am a template</p>'*/
 });
