65348d6c9202c7f573ca62305ca8c8cf19f15d58
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-30 10:41:53 +0100

    added lesson 13 code - punch bag

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 new Vue({
     el: '#vue-app',
     data: {
-        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-        ninjas: [
-            { name: 'Ryu', age: 25 },
-            { name: 'Yoshi', age: 35 },
-            { name: 'Ken', age: 55 }
-        ]
+        health: 100,
+        ended: false
     },
     methods: {
-
+        punch: function(){
+            this.health -= 10;
+            if ( this.health <= 0 ){
+                this.ended = true;
+            }
+        },
+        restart: function(){
+            this.health = 100;
+            this.ended = false;
+        }
     },
     computed: {
 
