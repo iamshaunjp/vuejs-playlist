f59f94fe9e7d77eb74dedc6c49e97276acfc88ed
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-03-30 15:36:46 +0100

    added lesson 16 code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-Vue.component('greeting', {
-    template: '<p>Hey there, I am a re-usable component</p>'
-});
-
 new Vue({
-    el: '.vue-app-one'
-    /*template: '<p>I am a template</p>'*/
+    el: '#vue-app-one',
+    data: {
+        output: 'Your favourite food'
+    },
+    methods: {
+        readRefs: function(){
+            console.log(this.$refs);
+            this.output = this.$refs.input.value;
+        }
+    }
 });
