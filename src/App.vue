e9e1bc783a3cd56b17a2a0e9b8c3620ac4dce230
iamshaunjp <shaunjp@shaun-pelling.com>
2017-03-31 15:00:57 +0100

    added lesson 22 code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <app-header></app-header>
-        <app-ninjas></app-ninjas>
+        <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
         <app-footer></app-footer>
     </div>
 </template>
@@ -20,7 +20,14 @@ export default {
     },
     data () {
         return {
-
+          ninjas: [
+              {name: 'Ryu', speciality: 'Vue Components', show: false},
+              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+              {name: 'Hitoshi', speciality: 'Click Events', show: false},
+              {name: 'Tango', speciality: 'Conditionals', show: false},
+              {name: 'Kami', speciality: 'Webpack', show: false},
+              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ]
         }
     }
 }
--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -10,16 +10,15 @@
 </template>
 <script>
 export default {
+    props: {
+      ninjas: {
+        type: Array,
+        required: true
+      }
+    },
     data(){
         return{
-            ninjas: [
-                {name: 'Ryu', speciality: 'Vue Components', show: false},
-                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-                {name: 'Hitoshi', speciality: 'Click Events', show: false},
-                {name: 'Tango', speciality: 'Conditionals', show: false},
-                {name: 'Kami', speciality: 'Webpack', show: false},
-                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-            ]
+
         }
     }
 }
