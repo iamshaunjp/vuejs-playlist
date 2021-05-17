2050f094c82d1a8d5ae0438e0e1890d702f4388e
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-05-17 22:07:42 +0200

    Complete lesson 15

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
-var one = new Vue({
+Vue.component('greeting', {
+  template: '<p>I am {{ name }} <button v-on:click="changeName">Change name</button></p>',
+  data: function(){
+    return {
+      name: "Yoshi"
+    }
+  },
+  methods: {
+    changeName: function(){
+      this.name = "Mario"
+    }
+  }
+});
+
+const one = new Vue({
     el: '#vue-app-one',
     data: {
-      title: 'Vue App One'
-    },
-    computed: {
-      greet: function(){
-        return 'Hello, from app one :)';
-      }
-    }
-});
-
-var two = new Vue({
-    el: '#vue-app-two',
-    data: {
-      title: 'Vue App Two'
-    },
-    computed: {
-      greet: function(){
-        return 'Yo dudes, this is app 2 speaking to ya';
-      }
+      title: 'Vue app one'
     },
     methods: {
-      changeTitle: function(){
-        one.title = 'Title Changed';
-      }
+
+    },
+    computed: {
+     greet: function(){
+       return 'hello from app 1';
+     }
     }
 });
 
-two.title = 'Changed from outside';
+const two = new Vue({
+  el: '#vue-app-two',
+  data: {
+    title: 'Vue app one'
+  },
+  methods: {
+    changeTitle: function(){
+      one.title = "Title changed";
+    }
+  },
+  computed: {
+    greet: function(){
+      return 'This is app 2';
+    }
+  }
+});
+
+two.title = "Change from outside"
